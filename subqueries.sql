e0fd2709c3f5984f4d9e2cacb19816fbee8ec3df
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-14 23:44:13 +0100

    SUBQUERIES 03

--- subqueries.sql
+++ subqueries.sql
@@ -1,9 +1,9 @@
 -- Challenge C : SUBQUERIES
 
--- 01 List products that cost above the average product price.
+-- 01 List products that cost above the average product price. -------------
 select * from products WHERE price > (SELECT  AVG(price) from products) ;
 
--- 02 Find customers who placed more orders than the average customer.
+-- 02 Find customers who placed more orders than the average customer. -------------
 
 -- get orders per customer
 SELECT customer_id, COUNT(*) AS order_count
@@ -15,7 +15,7 @@ SELECT AVG(order_count) FROM (SELECT customer_id, COUNT(*) AS order_count
 FROM orders
 GROUP BY customer_id) t;
 
-
+-- result: 
 SELECT c.customer_id, c.first_name, c.last_name
 FROM customers c
 JOIN (
@@ -30,5 +30,33 @@ WHERE opc.order_count >
         SELECT COUNT(*) AS order_count
         FROM orders
         GROUP BY customer_id
+    ) avg_orders
+);
+
+-- 03 List orders whose total amount is above average order value. -------------
+SELECT * FROM order_items ;
+-- calculate order_total
+SELECT order_id,SUM(price*quantity) as order_total FROM order_items GROUP BY order_id;
+-- calculate avg of order_total
+SELECT AVG(order_total) as avg_orders FROM (
+        SELECT SUM(price * quantity) AS order_total
+        FROM order_items
+        GROUP BY order_id
+    ) avg_orders;
+
+-- result:
+SELECT sp.order_id, sp.total_price
+FROM (
+    SELECT order_id, SUM(price * quantity) AS total_price
+    FROM order_items
+    GROUP BY order_id
+) sp
+WHERE sp.total_price >
+(
+    SELECT AVG(order_total)
+    FROM (
+        SELECT SUM(price * quantity) AS order_total
+        FROM order_items
+        GROUP BY order_id
     ) avg_orders
 );
